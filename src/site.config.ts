355b579a27090f9b8a844c7490d3d9a8b91af7c1
cooloozhang <cooloozhang@foxmail.com>
2023-09-11 18:31:00 +0800

    no message

--- src/site.config.ts
+++ src/site.config.ts
@@ -2,11 +2,11 @@ import type { SiteConfig } from "@/data/types";
 
 export const siteConfig: SiteConfig = {
 	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
-	author: "Chris Williams",
+	author: "cooloozhang",
 	// Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
-	title: "Astro Theme Cactus",
+	title: "小小前端一枚~~",
 	// Meta property used as the default description meta property
-	description: "An opinionated starter theme for Astro",
+	description: "小小前端，天天搬砖，维护屎山",
 	// HTML lang property, found in src/layouts/Base.astro L:18
 	lang: "en-GB",
 	// Meta property, found in src/components/BaseHead.astro L:42
