636a215255c369d748df9f34237bc8bba4233318
cooloozhang <cooloozhang@foxmail.com>
2023-09-11 17:10:20 +0800

    no message

--- astro.config.ts
+++ astro.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "astro/config";
-import fs from 'fs';
+import fs from "fs";
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
@@ -11,7 +11,8 @@ import { remarkReadingTime } from "./src/utils/remark-reading-time.mjs";
 // https://astro.build/config
 export default defineConfig({
 	// ! Please remember to replace the following site property with your own domain
-	site: "https://astro-theme-cactus.netlify.app/",
+	site: "https://coolooks.github.io",
+	base: "/blogs",
 	markdown: {
 		remarkPlugins: [remarkUnwrapImages, remarkReadingTime],
 		remarkRehype: { footnoteLabelProperties: { className: [""] } },
@@ -29,25 +30,25 @@ export default defineConfig({
 		prefetch(),
 	],
 	vite: {
-		plugins: [rawFonts(['.ttf'])],
+		plugins: [rawFonts([".ttf"])],
 		optimizeDeps: {
 			exclude: ["@resvg/resvg-js"],
 		},
 	},
 });
 
-function rawFonts(ext: Array<string>) {
-  return {
-    name: 'vite-plugin-raw-fonts',
+function rawFonts (ext: Array<string>) {
+	return {
+		name: "vite-plugin-raw-fonts",
 		// @ts-ignore:next-line
-    transform(_, id) {
-      if (ext.some(e => id.endsWith(e))) {
-        const buffer = fs.readFileSync(id);
-        return {
-          code: `export default ${JSON.stringify(buffer)}`,
-          map: null
-        };
-      }
-    }
-  };
+		transform (_, id) {
+			if (ext.some((e) => id.endsWith(e))) {
+				const buffer = fs.readFileSync(id);
+				return {
+					code: `export default ${JSON.stringify(buffer)}`,
+					map: null,
+				};
+			}
+		},
+	};
 }
