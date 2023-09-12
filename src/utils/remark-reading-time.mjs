b1fdfce1046432be8cced95b95507e1059779c4d
cooloozhang <cooloozhang@foxmail.com>
2023-09-12 17:34:38 +0800

    init

--- src/utils/remark-reading-time.mjs
+++ src/utils/remark-reading-time.mjs
@@ -1,10 +1,10 @@
 import getReadingTime from "reading-time";
 import { toString } from "mdast-util-to-string";
 
-export function remarkReadingTime() {
+export function remarkReadingTime () {
 	return function (tree, { data }) {
 		const textOnPage = toString(tree);
-		const readingTime = getReadingTime(textOnPage);
-		data.astro.frontmatter.minutesRead = readingTime.text;
+		const { minutes, words } = getReadingTime(textOnPage);
+		data.astro.frontmatter.minutesRead = `共${words}字，阅读需 ${Math.ceil(minutes)} 分钟`;
 	};
 }
